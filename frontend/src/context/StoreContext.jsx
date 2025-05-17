29ca36fe008b22992d6fff29ad2bd6cd7bd673ff
Nikhil Farand <nikhilfarand@Nikhils-MacBook-Air-5.local>
2025-05-17 19:14:10 +0530

    updated frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,15 +1,42 @@
-import { createContext } from "react"
+import { createContext, useState, useEffect } from "react"
 import { food_list } from "../assets/assets"
 
 export const StoreContext = createContext(null)
 
-
 const StoreContextProvider = (props) => {
 
+    const [cartItems, setCartItems] = useState({})
 
+    const addToCart =(itemId)=>{
+        if(!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
+    const removeFromCart =(itemId)=>{
+        if(cartItems[itemId] > 0) {
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+            if(cartItems[itemId] === 1) {
+                const newCartItems = {...cartItems}
+                delete newCartItems[itemId]
+                setCartItems(newCartItems)
+            }
+        }
+    }
+
+    useEffect(()=>{
+        console.log(cartItems)
+    },[cartItems])
 
     const contextValue ={
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     return(
         <StoreContext.Provider value={contextValue}>
